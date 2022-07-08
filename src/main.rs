48cb6fca80a22ef788de8a480e83bb094d952eb3
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-07-08 14:02:40 +0200

    Fixed X winning instead of a draw + reformatting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-use std::{io, collections::HashMap};
+use std::io;
 
 fn main() {
-    let (mut x, mut y): (usize, &str);
-    let dict_y: HashMap<&str, usize> = HashMap::from([
-        ("a", 1),
-        ("b", 2),
-        ("c", 3)
-    ]);
+
+    let (mut x, mut y): (usize, char);
     let mut counter: usize;
-    let mut xo: [[char; 3]; 3];
+    let mut field: [[char; 3]; 3];
+
     loop {
         counter = 0;
-        xo = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ', ]];
-        draw(&xo);
+        field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ', ]];
+        draw(&field);
         loop {
             let mut input: String = String::new();
             io::stdin()
@@ -20,14 +17,20 @@ fn main() {
                 .expect("Input failed");
 
             // print!("{} {}", &input[2..], &input[..1]);
-            (x, y) = (input[..1].parse::<usize>().unwrap(), &input[1..2]);
+            (x, y) = (input[..1].parse::<usize>().unwrap(), input.chars().nth(1).unwrap());
+            let y:usize = y as usize - 96;
             let current = ['X', 'O'][counter % 2];
-            xo[x-1][dict_y[y]-1] = current;
+            field[x-1][y-1] = current;
 
-            draw(&xo);
+            draw(&field);
             counter += 1;
-            if (counter == 9) | check(&xo, x-1, dict_y[y]-1) {
-                println!("{} won!", current);
+            if counter == 9 {
+
+                if check(&field, x-1, y-1) {
+                    println!("{} won!", current);
+                    break
+                }
+                println!("Draw!");
                 break
             }
         }
@@ -35,25 +38,26 @@ fn main() {
     }
 }
 
-fn check(xo: &[[char; 3]; 3], x: usize, y: usize) -> bool {
+fn check(field: &[[char; 3]; 3], x: usize, y: usize) -> bool {
     let diagonal = if x == y {
-        xo[0][0] == xo[1][1] && xo[1][1] == xo[2][2]
+        field[0][0] == field[1][1] && field[1][1] == field[2][2]
     }
     else if x + y == 2 {
-        xo[0][2] == xo[1][1] && xo[1][1] == xo[2][0]
+        field[0][2] == field[1][1] && field[1][1] == field[2][0]
     }
     else {
         false
     };
-    ((xo[0][y] == xo[1][y] && xo[1][y] == xo[2][y]) | (xo[x][0] == xo[x][1] && xo[x][1] == xo[x][2])) | diagonal
+    ((field[0][y] == field[1][y] && field[1][y] == field[2][y]) | (field[x][0] == field[x][1] && field[x][1] == field[x][2])) | diagonal
 }
 
-fn draw(xo: &[[char; 3]; 3]) {
-            println!("   |   |   ");
-            println!(" {} | {} | {} ", xo[0][0], xo[0][1], xo[0][2]);
-            println!("---|---|---");
-            println!(" {} | {} | {} ", xo[1][0], xo[1][1], xo[1][2]);
-            println!("---|---|---");
-            println!(" {} | {} | {} ", xo[2][0], xo[2][1], xo[2][2]);
-            println!("   |   |   ");
+fn draw(field: &[[char; 3]; 3]) {
+            println!("   a   b   c ");
+            println!("     |   |   ");
+            println!("1  {} | {} | {} ", field[0][0], field[0][1], field[0][2]);
+            println!("  ---|---|---");
+            println!("2  {} | {} | {} ", field[1][0], field[1][1], field[1][2]);
+            println!("  ---|---|---");
+            println!("3  {} | {} | {} ", field[2][0], field[2][1], field[2][2]);
+            println!("     |   |   ");
 }
