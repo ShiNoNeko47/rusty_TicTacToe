d5a0159b8abd56560d8a91d8c1f527dd64c1375e
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-07-08 14:11:33 +0200

    lsp formatting

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,61 @@
 use std::io;
 
 fn main() {
-
     let (mut x, mut y): (usize, char);
     let mut counter: usize;
     let mut field: [[char; 3]; 3];
 
     loop {
         counter = 0;
-        field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ', ]];
+        field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
         draw(&field);
         loop {
             let mut input: String = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Input failed");
+            io::stdin().read_line(&mut input).expect("Input failed");
 
             // print!("{} {}", &input[2..], &input[..1]);
-            (x, y) = (input[..1].parse::<usize>().unwrap(), input.chars().nth(1).unwrap());
-            let y:usize = y as usize - 96;
+            (x, y) = (
+                input[..1].parse::<usize>().unwrap(),
+                input.chars().nth(1).unwrap(),
+            );
+            let y: usize = y as usize - 96;
             let current = ['X', 'O'][counter % 2];
-            field[x-1][y-1] = current;
+            field[x - 1][y - 1] = current;
 
             draw(&field);
             counter += 1;
             if counter == 9 {
-
-                if check(&field, x-1, y-1) {
+                if check(&field, x - 1, y - 1) {
                     println!("{} won!", current);
-                    break
+                    break;
                 }
                 println!("Draw!");
-                break
+                break;
             }
         }
-        break
     }
 }
 
 fn check(field: &[[char; 3]; 3], x: usize, y: usize) -> bool {
     let diagonal = if x == y {
         field[0][0] == field[1][1] && field[1][1] == field[2][2]
-    }
-    else if x + y == 2 {
+    } else if x + y == 2 {
         field[0][2] == field[1][1] && field[1][1] == field[2][0]
-    }
-    else {
+    } else {
         false
     };
-    ((field[0][y] == field[1][y] && field[1][y] == field[2][y]) | (field[x][0] == field[x][1] && field[x][1] == field[x][2])) | diagonal
+    ((field[0][y] == field[1][y] && field[1][y] == field[2][y])
+        | (field[x][0] == field[x][1] && field[x][1] == field[x][2]))
+        | diagonal
 }
 
 fn draw(field: &[[char; 3]; 3]) {
-            println!("   a   b   c ");
-            println!("     |   |   ");
-            println!("1  {} | {} | {} ", field[0][0], field[0][1], field[0][2]);
-            println!("  ---|---|---");
-            println!("2  {} | {} | {} ", field[1][0], field[1][1], field[1][2]);
-            println!("  ---|---|---");
-            println!("3  {} | {} | {} ", field[2][0], field[2][1], field[2][2]);
-            println!("     |   |   ");
+    println!("   a   b   c ");
+    println!("     |   |   ");
+    println!("1  {} | {} | {} ", field[0][0], field[0][1], field[0][2]);
+    println!("  ---|---|---");
+    println!("2  {} | {} | {} ", field[1][0], field[1][1], field[1][2]);
+    println!("  ---|---|---");
+    println!("3  {} | {} | {} ", field[2][0], field[2][1], field[2][2]);
+    println!("     |   |   ");
 }
