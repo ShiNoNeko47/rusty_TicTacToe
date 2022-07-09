48862582ec37f0487a1b28e18a4466b598e279ca
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-07-09 21:41:12 +0200

    fixed input error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::io;
 
 fn main() {
-    let (mut x, mut y): (usize, char);
     let mut counter: usize;
     let mut field: [[char; 3]; 3];
 
@@ -10,25 +9,41 @@ fn main() {
         field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
         draw(&field);
         loop {
-            let mut input: String = String::new();
-            io::stdin().read_line(&mut input).expect("Input failed");
-
-            // print!("{} {}", &input[2..], &input[..1]);
-            (x, y) = (
-                input[..1].parse::<usize>().unwrap(),
-                input.chars().nth(1).unwrap(),
-            );
-            let y: usize = y as usize - 96;
             let current = ['X', 'O'][counter % 2];
-            field[x - 1][y - 1] = current;
+            let (x, y): (usize, char);
+            let mut input: String = String::new();
 
-            draw(&field);
-            counter += 1;
-            if counter == 9 {
-                if check(&field, x - 1, y - 1) {
-                    println!("{} won!", current);
-                    break;
+            println!("{}: ", current);
+            io::stdin().read_line(&mut input).expect("Input failed");
+            x = match input[..1].parse() {
+                Ok(n) => {
+                    if (n < 1) | (n > 3) {
+                        println!("Invalid number");
+                        continue;
+                    }
+                    n
                 }
+                Err(err) => {
+                    println!("{}", err);
+                    continue;
+                }
+            };
+            y = input.chars().nth(1).unwrap();
+            if (y < 'a') | (y > 'c') {
+                continue;
+            }
+            let y: usize = y as usize - 96;
+
+            field[x - 1][y - 1] = current;
+            draw(&field);
+
+            counter += 1;
+
+            if check(&field, x - 1, y - 1) {
+                println!("{} won!", current);
+                break;
+            }
+            if counter == 9 {
                 println!("Draw!");
                 break;
             }
